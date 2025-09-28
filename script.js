07a47aaeea275d1958c12c24a1ade455e6f1820b
Cashtira <22520599@gm.uit.edu.vn>
2025-09-28 22:51:30 +0700

    Add detail for Script + Projects section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Bấm nút mở menu trên điện thoại
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.getElementById('hamburger');
   const nav = document.getElementById('main-nav');
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+// Slide tự chạy
 let currentIndex = 0;
 const slides = document.querySelector(".slides");
 const slide = document.querySelectorAll(".slide");
@@ -28,3 +30,33 @@ setInterval(() => {
 }, 4000);
 
 showSlide(0)
+
+// Bấm nút hiện form
+const modal = document.getElementById("joinModal");
+const closeBtn = modal.querySelector(".close");
+
+const openButtons = document.querySelectorAll(".open-modal");
+
+openButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    modal.style.display = "flex";
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+const form = modal.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // chặn gửi đi
+  alert("Thank you for joining!"); 
+  modal.style.display = "none";
+  form.reset();
+});
